358e2bad5f3ad11aa0da870f2c7ea844c9f1aa64
nyhvignoli <nyhv.contato@gmail.com>
2021-03-13 14:58:25 -0300

    Configurando cors, express e criando o servidor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello wolrd");
+import cors from 'cors';
+import express, { Express } from 'express';
+import { AddressInfo } from 'net';
+
+// Cors and Express config
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+
+// Server config
+const server = app.listen( process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+        console.log(`Failure upon running server`);
+    }
+});
